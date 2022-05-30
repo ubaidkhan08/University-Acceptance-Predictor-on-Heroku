a2e37588cf374850e57dcab19056ab21f50b9b0c
ubaidkhan08 <88443025+ubaidkhan08@users.noreply.github.com>
2022-05-31 03:04:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -345,6 +345,7 @@ def classify(gre,tofel,sepal_length, sepal_width, petal_length, petal_width,rese
     #df = df.drop('Serial No.', axis=1)
     #XX = df.drop('Chance of Admit', axis=1)
 
+    XX = df
     XX.iloc[0] = [inputs]
     XX = scalerrr.fit_transform(XX)
 
